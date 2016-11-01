01fca5feca63d5aac367a784b555a0883232d6cd
Jeff Szusz <jeff.szusz@gmail.com>
2016-10-31 20:11:32 -0400

    change direction to use the create-react-app tool to bootstrap the project

--- src/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import filmList from './wiff.json';
-
-import Layout from './layout.jsx';
-
-render(<Layout/>, document.getElementById('application'));
-
